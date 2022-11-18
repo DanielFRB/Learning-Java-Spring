ee4d874cc59ce8d26a41fd52c910678dec851823
Daniel Rondon <danfelrondon@gmail.com>
2022-11-18 19:39:08 -0400

    Primeros pasos: Tipos de Datos, Oper Basicas, String Equals, String Invert, Palindromo

---
+++ Java/Fundamentos/src/com/primerospasos/DataTypes.java
@@ -0,0 +1,28 @@
+package com.primerospasos;
+
+/**
+ * @author danfe
+ */
+
+public class DataTypes {
+    public static void main(String[] args) {
+        //Datos Primitivos
+        System.out.println("Datos Primitivos");
+        byte a = 126;
+        System.out.println("byte: " + a);
+        short b = 10000;
+        System.out.println("short: " + b);
+        int c = 123456789;
+        System.out.println("int: " + c);
+        long d = 9223372036854775805l;
+        System.out.println("long: " + d);
+        float e = 32.1451f;
+        System.out.println("float: " + e);
+        double f = 45.6547875435156465;
+        System.out.println("double: " + f);
+        char g = 'w';
+        System.out.println("char: " + g);
+        boolean h = true;
+        System.out.println("boolean: " + h);
+    }
+}
