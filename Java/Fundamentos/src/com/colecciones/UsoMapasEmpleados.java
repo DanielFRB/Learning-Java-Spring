c988cda498c989342f352bd2c1389df4fb6fda56
Daniel Rondon <danfelrondon@gmail.com>
2022-11-22 16:41:22 -0400

    Colecciones : Actualizacion del Ejemplo UsoMapasEmpleados, se agregan varias formas de imprimir la informacion contenida en el Mapa

--- Java/Fundamentos/src/com/colecciones/UsoMapasEmpleados.java
+++ Java/Fundamentos/src/com/colecciones/UsoMapasEmpleados.java
@@ -1,6 +1,7 @@
 package com.colecciones;
 
 import java.util.*;
+import java.util.function.Consumer;
 
 /**
  * @author danfe
@@ -14,14 +15,38 @@ public class UsoMapasEmpleados {
         personal.put("147", new EmpleadoM("Oswaldo"));
         personal.put("148", new EmpleadoM("Barack"));
         System.out.println(personal);
+        System.out.println("--------");
         
         personal.remove("145");
         System.out.println(personal);
         personal.put("148", new EmpleadoM("Maritza"));
         System.out.println(personal);
+        System.out.println("--------");
         
         for (Map.Entry<String, EmpleadoM> p : personal.entrySet()) {
             System.out.println("Clave: "+ p.getKey() +", Valor: "+ p.getValue());
         }
+        System.out.println("--------");
+        
+        // Usando Iteradores
+        for (Iterator<Map.Entry<String, EmpleadoM>> it = personal.entrySet().iterator(); it.hasNext();) {
+            Map.Entry<String, EmpleadoM> p = it.next();
+            System.out.println("Clave: "+ p.getKey() +", Valor: "+ p.getValue());
+        }
+        System.out.println("--------");
+        
+        // Usando Operaciones Funcionales con lambdas
+        personal.entrySet().forEach(p -> {
+            System.out.println("Clave: "+ p.getKey() +", Valor: "+ p.getValue());
+        });
+        System.out.println("--------");
+        
+        // Usando clases anonimas internas
+        personal.entrySet().forEach(new Consumer<Map.Entry<String, EmpleadoM>>() {
+            @Override
+            public void accept(Map.Entry<String, EmpleadoM> p) {
+                System.out.println("Clave: "+ p.getKey() +", Valor: "+ p.getValue());
+            }
+        });
     }
 }
