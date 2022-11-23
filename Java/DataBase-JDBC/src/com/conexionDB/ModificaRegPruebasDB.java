f66051953489a38f18322fe7553a2dd95cf57169
Daniel Rondon <danfelrondon@gmail.com>
2022-11-23 15:37:02 -0400

    JDBC - Ejemplo sobre Operaciones Insert, Update y Delete a una BD

---
+++ Java/DataBase-JDBC/src/com/conexionDB/ModificaRegPruebasDB.java
@@ -0,0 +1,26 @@
+package com.conexionDB;
+
+import java.sql.*;
+
+/**
+ * @author danfe
+ */
+
+public class ModificaRegPruebasDB {
+    public static void main(String[] args) {
+        try {
+            Connection miConexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/pruebas", "root", "");
+            Statement miStatement = miConexion.createStatement();
+            String sentenciaSql = "INSERT INTO PRODUCTOS (codArticulo, seccion, nombreArticulo, precio, fecha, importado, paisDeOrigen) VALUES ('ACD030', 'DEPORTES', 'BOTINES PARA BASE BALL', 128.60, '2022-08-01', '1', 'USA')";
+            //String sentenciaSql = "UPDATE PRODUCTOS SET PRECIO=PRECIO*2 WHERE codArticulo = 'ACD012'";
+            //String sentenciaSql = "DELETE FROM PRODUCTOS WHERE codArticulo = 'ACD017'";
+            miStatement.executeUpdate(sentenciaSql);
+            System.out.println("Datos Ingresados Correctamente a la tabla PRODUCTOS");
+            //System.out.println("Precio del Producto Actualizado");
+            //System.out.println("Producto Eliminado");
+        } catch (Exception e) {
+            System.out.println("No se pudo establecer la Conexion con la BD");
+            e.printStackTrace();
+        }
+    }
+}
