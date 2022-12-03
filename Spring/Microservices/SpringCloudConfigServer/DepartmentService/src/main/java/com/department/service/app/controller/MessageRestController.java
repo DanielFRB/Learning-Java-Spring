b2640d28b38685e8e162082dfb60f9d5f13b4d7d
Daniel Rondon <danfelrondon@gmail.com>
2022-12-03 18:43:14 -0400

    Servicio DepartmentService para Ejm. de Mic. Serv. SC Config Server

---
+++ Spring/Microservices/SpringCloudConfigServer/DepartmentService/src/main/java/com/department/service/app/controller/MessageRestController.java
@@ -0,0 +1,18 @@
+package com.department.service.app.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class MessageRestController {
+    @Value("${message : Hello default}")
+    private String message;
+
+    @GetMapping("/mensaje")
+    String getMessage() {
+        return this.message;
+    }
+}
