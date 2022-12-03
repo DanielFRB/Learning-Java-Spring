77c6d43eb074dc7fdd0633ff0a00c4ef69fd0270
Daniel Rondon <danfelrondon@gmail.com>
2022-12-03 13:17:55 -0400

    Servicio de Registro a Eureka para el Ejem. de Mic. Serv. SC Netflix Eureka

---
+++ Spring/Microservices/SpringCloudNetflixEureka/Service-Registry/src/test/java/com/dfrb/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfrb.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
