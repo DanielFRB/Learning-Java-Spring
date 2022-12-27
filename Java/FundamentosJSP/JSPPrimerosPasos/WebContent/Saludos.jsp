3d262f1f4a0b1614ceb80dc28046b25d215c82ed
Daniel Rondon <danfelrondon@gmail.com>
2022-12-27 18:12:59 -0400

    Primeros Ejemplos de uso de JSP

---
+++ Java/FundamentosJSP/JSPPrimerosPasos/WebContent/Saludos.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Mi Primer JSP</title>
+</head>
+<body>
+	<h1>Primer JSP</h1>
+	<p>La Fecha y Hora del Sistema es: <%= new java.util.Date() %></p>
+</body>
+</html>
---
+++ Java/FundamentosJSP/JSPPrimerosPasos/src/com/dfrb/java/jsp/Calculos.java
@@ -0,0 +1,17 @@
+package com.dfrb.java.jsp;
+
+public class Calculos {
+	private static int result;
+
+	public static int suma(int x, int y) {
+		return result = x+y;
+	}
+	
+	public static int resta(int x, int y) {
+		return result = x-y;
+	}
+	
+	public static int multiplica(int x, int y) {
+		return result = x*y;
+	}
+}
